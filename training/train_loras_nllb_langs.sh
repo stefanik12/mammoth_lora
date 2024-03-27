35deca89a58384d944fe92d0fd7a852dffadeb70
stefanik12 <stefanik.mich@gmail.com>
2024-03-27 15:28:18 +0200

    System vars

--- training/train_loras_nllb_langs.sh
+++ training/train_loras_nllb_langs.sh
@@ -13,4 +13,11 @@ pip install -r training/requirements.txt
 export CUDA_VISIBLE_DEVICES=0
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 
-srun python training/train_loras_nllb_langs.sh
+export WANDB_API_KEY=XXX
+export WANDB_PROJECT=mammoth-lora
+
+# puhti data config:
+export DATA_DIR=/scratch/project_2001194/tiedeman/Tatoeba-Challenge/data/release/v2023-09-26
+export HF_MODEL_ID=facebook/nllb-200-distilled-600M
+
+srun python training/train_loras_nllb_langs.sh --base_data_dir ${DATA_DIR} --base_model ${HF_MODEL_ID}
