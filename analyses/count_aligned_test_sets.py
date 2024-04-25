ae5cc3a215a197e6e334a55e0e7c27ab61b06f84
stefanik12 <stefanik.mich@gmail.com>
2024-04-25 11:34:34 +0300

    analytics: count aligned test sets

--- analyses/count_aligned_test_sets.py
+++ analyses/count_aligned_test_sets.py
@@ -19,18 +19,21 @@ file_template = base_data_dir + "/eng-%s/test.%s"
 already_evaled = set()
 
 for train_lang in training_langs:
+    if not os.path.exists(file_template % (train_lang, "src")):
+        continue
     train_src_all = list(AdaptationDataset.iter_text_file_per_line(file_template % (train_lang, "src")))
-    train_tgt_all = list(AdaptationDataset.iter_text_file_per_line(file_template % (train_lang, "src")))
+    train_tgt_all = list(AdaptationDataset.iter_text_file_per_line(file_template % (train_lang, "trg")))
+
     for eval_lang in eval_langs:
+        if not os.path.exists(file_template % (eval_lang, "src")):
+            continue
         if "%s-%s" % (train_lang, eval_lang) in already_evaled:
             continue
         eval_src_all = list(AdaptationDataset.iter_text_file_per_line(file_template % (eval_lang, "src")))
-        eval_tgt_all = list(AdaptationDataset.iter_text_file_per_line(file_template % (eval_lang, "src")))
-        if os.path.exists(file_template % (train_lang, "src")):
-            train_tgt, eval_tgt = find_aligned_by_index(train_src_all, train_tgt_all, eval_src_all, eval_tgt_all)
-            num_shared = len(train_tgt)
-        else:
-            num_shared = 0
+        eval_tgt_all = list(AdaptationDataset.iter_text_file_per_line(file_template % (eval_lang, "trg")))
+
+        train_tgt, eval_tgt = find_aligned_by_index(train_src_all, train_tgt_all, eval_src_all, eval_tgt_all)
+        num_shared = len(train_tgt)
 
         print("%s and %s share %s test samples" % (train_lang, eval_lang, num_shared))
         already_evaled.add("%s-%s" % (train_lang, eval_lang))
